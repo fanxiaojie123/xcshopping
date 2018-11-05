3970b63c59f4623b45d4736f280096396194604a
fxj <1136829130@qq.com>
2018-11-05 11:20:09 +0800

    pojo

---
+++ xc-shopping-provider/src/main/java/cn/axy/xc/xcshoppingprovider/pojo/XcOrderO.java
@@ -0,0 +1,205 @@
+package cn.axy.xc.xcshoppingprovider.pojo;
+
+import java.util.Date;
+
+public class XcOrderO {
+    private String orderId;
+
+    private String payment;
+
+    private Integer paymentType;
+
+    private String postFee;
+
+    private Integer status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Date paymentTime;
+
+    private Date consignTime;
+
+    private Date endTime;
+
+    private Date closeTime;
+
+    private String shippingName;
+
+    private String shippingCode;
+
+    private String userId;
+
+    private String buyerMessage;
+
+    private String buyerNick;
+
+    private Integer buyerRate;
+
+    private String extend1;
+
+    private String extend2;
+
+    private String extend3;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId == null ? null : orderId.trim();
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment == null ? null : payment.trim();
+    }
+
+    public Integer getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(Integer paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public String getPostFee() {
+        return postFee;
+    }
+
+    public void setPostFee(String postFee) {
+        this.postFee = postFee == null ? null : postFee.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getPaymentTime() {
+        return paymentTime;
+    }
+
+    public void setPaymentTime(Date paymentTime) {
+        this.paymentTime = paymentTime;
+    }
+
+    public Date getConsignTime() {
+        return consignTime;
+    }
+
+    public void setConsignTime(Date consignTime) {
+        this.consignTime = consignTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(Date closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    public String getShippingName() {
+        return shippingName;
+    }
+
+    public void setShippingName(String shippingName) {
+        this.shippingName = shippingName == null ? null : shippingName.trim();
+    }
+
+    public String getShippingCode() {
+        return shippingCode;
+    }
+
+    public void setShippingCode(String shippingCode) {
+        this.shippingCode = shippingCode == null ? null : shippingCode.trim();
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getBuyerMessage() {
+        return buyerMessage;
+    }
+
+    public void setBuyerMessage(String buyerMessage) {
+        this.buyerMessage = buyerMessage == null ? null : buyerMessage.trim();
+    }
+
+    public String getBuyerNick() {
+        return buyerNick;
+    }
+
+    public void setBuyerNick(String buyerNick) {
+        this.buyerNick = buyerNick == null ? null : buyerNick.trim();
+    }
+
+    public Integer getBuyerRate() {
+        return buyerRate;
+    }
+
+    public void setBuyerRate(Integer buyerRate) {
+        this.buyerRate = buyerRate;
+    }
+
+    public String getExtend1() {
+        return extend1;
+    }
+
+    public void setExtend1(String extend1) {
+        this.extend1 = extend1 == null ? null : extend1.trim();
+    }
+
+    public String getExtend2() {
+        return extend2;
+    }
+
+    public void setExtend2(String extend2) {
+        this.extend2 = extend2 == null ? null : extend2.trim();
+    }
+
+    public String getExtend3() {
+        return extend3;
+    }
+
+    public void setExtend3(String extend3) {
+        this.extend3 = extend3 == null ? null : extend3.trim();
+    }
+}
