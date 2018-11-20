008b5a739eed7e72f61c041345d726f557d5040f
zhangli <1198285654@qq.com>
2018-11-20 08:38:54 +0800

    展示的提供者7

--- xc-item-detail-consumer/src/main/java/cn/axy/xc/xcitemdetailconsumer/service/InventoryQueue.java
+++ xc-item-detail-consumer/src/main/java/cn/axy/xc/xcitemdetailconsumer/service/InventoryQueue.java
@@ -1,5 +1,16 @@
 package cn.axy.xc.xcitemdetailconsumer.service;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Repository
 public interface InventoryQueue {
+    @RequestMapping(value = "/updmessageinv",method = RequestMethod.POST)
+    String updmessageinv(@RequestParam("mid")Integer mid);
+    @RequestMapping(value = "/updskuinv",method = RequestMethod.POST)
+    String updskuinv(@RequestParam("商品ID")Integer mid,
+                     @RequestParam("SKUID")Integer skuid);
 
 }
