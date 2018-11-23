56fe6cc672d52063395f5c1f48874d288f4c366c
zhangli <1198285654@qq.com>
2018-11-23 22:28:03 +0800

    展示的提供者8

--- xc-item-detail-consumer/src/main/java/cn/axy/xc/xcitemdetailconsumer/config/InventoryQconfig.java
+++ xc-item-detail-consumer/src/main/java/cn/axy/xc/xcitemdetailconsumer/config/InventoryQconfig.java
@@ -1,8 +1,13 @@
 package cn.axy.xc.xcitemdetailconsumer.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@Configuration
 public class InventoryQconfig {
     //正常的消费者的队列
     public static  final  String  CONSUME_QUEUE  = "consume-queue";
@@ -11,4 +16,11 @@ public class InventoryQconfig {
     public Queue queue(){
         return new Queue(CONSUME_QUEUE,true,false,false);
     }
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+//    @Scheduled(fixedRate = 50000)//5秒  initialDelay 程序启动后延迟
+//    public Queue send(){
+//        return rabbitTemplate.convertAndSend(InventoryQconfig.CONSUME_QUEUE,);
+//    }
 }
