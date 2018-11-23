3a4538f85e5fb23fe44983f3f10d111131bf32fc
zhangli <1198285654@qq.com>
2018-11-23 18:54:17 +0800

    展示的提供者7

--- xc-item-show-consumer/src/main/java/cn/axy/xc/xcitemshowconsumer/XcItemShowConsumerApplication.java
+++ xc-item-show-consumer/src/main/java/cn/axy/xc/xcitemshowconsumer/XcItemShowConsumerApplication.java
@@ -2,17 +2,20 @@ package cn.axy.xc.xcitemshowconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SolrAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@EnableScheduling//boot开启定时任务注解
 public class XcItemShowConsumerApplication {
 
     public static void main(String[] args) {
