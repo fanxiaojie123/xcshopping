a70d549ae8c67fca36654173887a783074d61ba9
zhangli <1198285654@qq.com>
2018-11-27 11:50:41 +0800

    展示的提供者8

--- xc-item-detail-provider/src/main/java/cn/axy/xc/xcitemdetailprovider/service/Updinvservice.java
+++ xc-item-detail-provider/src/main/java/cn/axy/xc/xcitemdetailprovider/service/Updinvservice.java
@@ -3,4 +3,5 @@ package cn.axy.xc.xcitemdetailprovider.service;
 public interface Updinvservice {
     String updmessageinv(Integer mid,Integer pcs);
     String updskuinv(Integer mid,Long skuid,Integer pcs);
+    void send(String invMQ);
 }
